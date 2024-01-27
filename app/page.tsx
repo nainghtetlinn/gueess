d7dd0f3d51b24be26f4780ff82750b16d639381a
nainghtetlinn <naing.work.it@gmail.com>
2024-01-27 14:34:06 +0630

    create hero section for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Hero from './components/Hero'
 import { useAppDispatch } from '@/lib/hook'
 import { checkAnswer } from '@/lib/features/game'
 
@@ -7,14 +8,17 @@ export default function Home() {
   const dispatch = useAppDispatch()
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          dispatch(checkAnswer({ answer: 3 }))
-        }}
-      >
-        Click
-      </button>
-    </div>
+    <>
+      <Hero />
+      <div>
+        <button
+          onClick={() => {
+            dispatch(checkAnswer({ answer: 3 }))
+          }}
+        >
+          Click
+        </button>
+      </div>
+    </>
   )
 }
