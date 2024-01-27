95f5e8e387dcb712aee0446dffd861f49ccce4da
nainghtetlinn <naing.work.it@gmail.com>
2024-01-27 13:40:54 +0630

    create app navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,27 @@
-import type { Metadata } from "next";
-import { Quicksand } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Quicksand } from 'next/font/google'
+import './globals.css'
 
-import StoreProvider from "./StoreProvider";
-import AppNav from "./components/nav/AppNav";
+import StoreProvider from './StoreProvider'
+import AppNav from './components/AppNav'
 
-const inter = Quicksand({ subsets: ["latin"] });
+const inter = Quicksand({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Gueess",
-  description: "Guess the hidden number",
-};
+  title: 'Gueess',
+  description: 'Guess the hidden number',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en" data-theme="lofi">
+    <html
+      lang='en'
+      data-theme='lofi'
+    >
       <body className={inter.className}>
         <StoreProvider>
           <AppNav />
@@ -26,5 +29,5 @@ export default function RootLayout({
         </StoreProvider>
       </body>
     </html>
-  );
+  )
 }
